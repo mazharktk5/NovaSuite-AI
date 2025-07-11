b8ed3fdafdaa842deee147df1d8ce385bedd0d1d
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-11 13:02:09 +0500

    added navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     return (
-        <div>
-            <h2>home page</h2>
-        </div>
+        <>
+            <Navbar />
+
+        </>
     )
 }
 
