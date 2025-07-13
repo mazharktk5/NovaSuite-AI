3a4b79d63b0ef5ddec27bce4ef8bebaa024fcd13
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-13 21:13:46 +0500

    integrated backent with write article

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,5 +1,14 @@
 import { Edit, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
+
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const WriteArticle = () => {
 
@@ -9,11 +18,39 @@ const WriteArticle = () => {
     { length: 1600, text: 'Long (1200+ words)' },
   ]
 
+
   const [selectedLength, setSelectedLength] = useState(articleLength[0])
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+
+  const { getToken } = useAuth()
 
   const onSubmitHandler = async (e) => {
     e.preventDefault()
+    try {
+
+      setLoading(true)
+      const prompt = `Write an article about ${input} in ${selectedLength.text}`
+
+      const { data } = await axios.post('/api/ai/generate-article', { prompt, length: selectedLength.length }, {
+        headers: {
+          'Authorization': `Bearer ${await getToken()}`
+        }
+      })
+
+      if (data.success) {
+        setContent(data.content)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+
+    }
+    setLoading(false)
+    setInput('')
   }
 
   return (
@@ -39,7 +76,13 @@ const WriteArticle = () => {
         </div>
 
         <br />
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'><Edit className='w-5' />
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> : <Edit className='w-5' />
+          }
+
+
           Generate Article
         </button>
 
@@ -56,14 +99,29 @@ const WriteArticle = () => {
 
         </div>
 
-        <div className='flex-1 flex justify-center items-center'>
 
-          <div className='text-sm flex flex-1 flex-col items-center gap-5 text-gray-400'>
-            <Edit className='w-9 h-9 ' />
-            <p>Enter a topic and Click "Generate Article" to get startded</p>
-          </div>
 
-        </div>
+        {
+          !content ? (
+            <div className='flex-1 flex justify-center items-center'>
+
+              <div className='text-sm flex flex-1 flex-col items-center gap-5 text-gray-400'>
+                <Edit className='w-9 h-9 ' />
+                <p>Enter a topic and Click "Generate Article" to get startded</p>
+              </div>
+
+            </div>
+          ) : (
+            <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+
+              <div className='reset-tw'>
+                <Markdown>{String(content).trim()}</Markdown>
+
+              </div>
+
+            </div>
+          )
+        }
 
       </div>
 
