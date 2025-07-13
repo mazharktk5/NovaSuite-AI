21920aa375d3a4f57e6deecf2c76e2b8ff988f86
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-13 12:38:47 +0500

    added ai controller

---
+++ server/controllers/aiController.js
@@ -0,0 +1,65 @@
+import OpenAI from "openai";
+import sql from "../configs/db.js";
+import { clerkClient } from "@clerk/express";
+
+const AI = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
+// generate Article
+
+export const generateArticle = async (req, res) => {
+    try {
+
+        const { userId } = req.auth();
+        const { prompt, length } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if (plan !== 'premium' && free_usage >= 0) {
+            return res.json({
+                success: false,
+                message: 'You have exceeded your free usage limit.Upgrade to continue',
+            })
+        }
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+            
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+
+            temperature: 0.7,
+            max_token:length,
+        });
+
+        const content = response.choices[0].message.content
+
+        await sql` INSERT INTO creations (user_id,prompt,content,type) VALUES(${userId},${prompt},${content},'article)`;
+
+        if(plan !== 'premium'){
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata:{ 
+                    free_usage: free_usage + 1
+                }
+            })
+        }
+
+        res.json({
+            success: true,
+            content: content
+
+        })
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ success: false, message:error.message });
+
+    }
+}
