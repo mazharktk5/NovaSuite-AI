3af24f6f3a9e5d367eea1f2c651bcb32bf85de64
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-14 17:24:45 +0500

    added remove background

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,16 +1,52 @@
 import { Eraser, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
 
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+
 const RemoveBackground = () => {
 
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+  const { getToken } = useAuth()
 
   const onSubmitHandler = async (e) => {
     e.preventDefault()
+    try {
+      setLoading(true)
+
+      const formData = new FormData()
+      formData.append('image', input)
+
+      const { data } = await axios.post('/api/ai/remove-image-background', formData, {
+        headers: {
+          'Authorization': `Bearer ${await getToken()}`
+        }
+      })
+
+      if (data.success) {
+        setContent(data.content)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+
+
+    }
+    setLoading(false)
+    setInput('')
   }
 
   return (
-   <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
 
       {/* left coloumn */}
       <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200' >
@@ -27,7 +63,12 @@ const RemoveBackground = () => {
         <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG,PNG and other image formats </p>
 
 
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'><Eraser className='w-5' />
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> : <Eraser className='w-5' />
+          }
+
+
           Remove Background
         </button>
 
@@ -44,18 +85,26 @@ const RemoveBackground = () => {
 
         </div>
 
-        <div className='flex-1 flex justify-center items-center'>
+        {
+          !content ? (
+            <div className='flex-1 flex justify-center items-center'>
 
-          <div className='text-sm flex flex-1 flex-col items-center gap-5 text-gray-400'>
-            <Eraser className='w-9 h-9 ' />
-            <p>Upload an image and click "Remove Background" to get started</p>
-          </div>
+              <div className='text-sm flex flex-1 flex-col items-center gap-5 text-gray-400'>
+                <Eraser className='w-9 h-9 ' />
+                <p>Upload an image and click "Remove Background" to get started</p>
+              </div>
 
-        </div>
+            </div>
+          ) : (
+            <img src={content} alt="" className='mt-3 w-full h-full' />
+          )
+        }
+
+
 
       </div>
 
-    </div> 
+    </div>
   )
 }
 
--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -193,7 +193,7 @@ export const removeImageBackground = async (req, res) => {
     try {
 
         const { userId } = req.auth();
-        const { image } = req.file;
+        const image = req.file;
         const plan = req.plan;
 
 
